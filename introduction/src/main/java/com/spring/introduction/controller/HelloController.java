47389a83b777f94e7415d85c8fcdfa365443f3fa
imjae <imjae950@gmail.com>
2023-11-22 14:07:41 +0900

    스프링 웹 기초 파악
    정적 컨텐츠, MVC와 템플릿 엔진, API 방식에대해 알아봄
    Controller와 html 파일을 이용해 테스트함.

---
+++ introduction/src/main/java/com/spring/introduction/controller/HelloController.java
@@ -0,0 +1,51 @@
+package com.spring.introduction.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data","controller!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name",name);
+
+        return "hello-template";
+    }
+
+    @GetMapping("hello-spring")
+    @ResponseBody //http 에서 body 에 return 값을 넣어준다는 의미이다.
+    public String helloSpring(@RequestParam("name") String name){
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; //객체를 넘기는 json 방식
+    }
+
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
